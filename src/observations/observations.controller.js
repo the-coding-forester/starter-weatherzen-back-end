48c6537d579a8ad143ad7ad6e530cbd1192e5196
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-27 11:00:58 -0400

    Set up observations controller file

---
+++ src/observations/observations.controller.js
@@ -0,0 +1,18 @@
+let nextId = 1;
+
+async function create(req, res) {
+  const newObservation = req.body.data;
+
+  const now = new Date().toISOString();
+  newObservation.observation_id = nextId++;
+  newObservation.created_at = now;
+  newObservation.updated_at = now;
+
+  res.status(201).json({
+    data: newObservation,
+  });
+}
+
+module.exports = {
+  create,
+};
