4977a874cb0b3cce3422a8fa525726a28e90b2f2
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-27 13:57:12 -0400

    Added GET to router

--- src/observations/observations.router.js
+++ src/observations/observations.router.js
@@ -2,6 +2,10 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./observations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
+router
+  .route("/")
+  .post(controller.create)
+  .get(controller.list)
+  .all(methodNotAllowed);
 
 module.exports = router;
