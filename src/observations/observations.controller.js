0001e6d61ede27092392c991eb218003d9020288
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-27 13:54:27 -0400

    Added middleware

--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,5 +1,39 @@
 let nextId = 1;
 
+const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
+
+function hasData(req, res, next) {
+  if (req.body.data) {
+    return next()
+  }
+  next({ status: 400, message: "body must have data property" })
+}
+
+function hasLatitude(req, res, next) {
+  const latitude = Number(req.body.data.latitude)
+  if (latitude >= -90 && latitude <= 90) {
+    return next()
+  }
+  next({ status: 400, message: "latitude must be between -90 and 90" })
+}
+
+function hasLongitude(req, res, next) {
+  const longitude = Number(req.body.data.longitude)
+  if (longitude >= -180 && longitude <= 180) {
+    return next()
+  }
+  next({ status: 400, message: "longitude must be between -180 and 180" })
+}
+
+function hasSkyCondition(req, res, next) {
+  const skyCondition = Number(req.body.data.sky_condition)
+
+  if (validSkyConditions.includes(skyCondition)) {
+    return next()
+  }
+  next({ status: 400, message: `sky_condition must be one of: ${validSkyConditions}` })
+}
+
 const create = async (req, res) => {
   const newObservation = req.body.data;
 
@@ -14,5 +48,11 @@ const create = async (req, res) => {
 }
 
 module.exports = {
-  create,
+  create: [
+    hasData,
+    hasLatitude,
+    hasLongitude,
+    hasSkyCondition,
+    create
+  ],
 };
